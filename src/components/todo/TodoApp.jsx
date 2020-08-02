1012655ba1cf42915695274a741efe2c84a10389
sina <pooya.sina@gmail.com>
2020-08-02 10:59:52 +0430

    add handle chane event when change username or password

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -11,11 +11,23 @@ class TodoApp extends Component{
 }
 
 class LoginComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username:'type a user name',
+            password:''
+        }
+    }
+    handleChanged=(event)=>{
+        console.log(event.target.value);
+        this.setState({[event.target.name]:event.target.value});
+    }
+    
     render(){
         return (
             <div>
-             User name :<input type="text" name="username"/>
-             password <input type="password" name="passwoed"/>   
+             User name :<input type="text" name="username" value={this.state.username} onChange={this.handleChanged}/>
+             password <input type="password" name="password" value={this.state.password} onChange={this.handleChanged}/>   
              <button>Loggin</button>
             </div>
         );
