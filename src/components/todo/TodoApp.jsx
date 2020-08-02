ffce3468522ab7b76e0cb9fe8d00a65faac0f66a
sina <pooya.sina@gmail.com>
2020-08-02 15:23:51 +0430

    add show message in todo app.jsx

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -15,23 +15,55 @@ class LoginComponent extends Component{
         super(props);
         this.state={
             username:'type a user name',
-            password:''
+            password:'',
+            showSuccessful:false,
+            showFailed:false
         }
     }
     handleChanged=(event)=>{
         console.log(event.target.value);
         this.setState({[event.target.name]:event.target.value});
     }
-    
+    loginClicked = ()=>{
+        if(this.state.username==='sina' && this.state.password==='pass' ){
+            this.setState({showSuccessful:true});
+            this.setState({showFailed:false});
+            console.log('login successful');
+        }else{
+            this.setState({showSuccessful:false});
+            this.setState({showFailed:true});    
+            console.log('login failed');
+        }
+    }
+    showSuccessfulMessage=()=>{
+        if(this.state.showSuccessful===true){
+            return <div>login successfull</div>;
+        }else{
+            return <div></div>;
+        }
+    }
+    showFailMessage=()=>{
+        if(this.state.showFailed===true){
+            return <div>login fail</div>;
+        }else{
+            return <div></div>;
+        }
+    }
     render(){
         return (
             <div>
-             User name :<input type="text" name="username" value={this.state.username} onChange={this.handleChanged}/>
-             password <input type="password" name="password" value={this.state.password} onChange={this.handleChanged}/>   
-             <button>Loggin</button>
+                {this.showSuccessfulMessage()}
+                {this.showFailMessage()}
+                
+                User name :<input type="text" name="username" value={this.state.username}
+                 onChange={this.handleChanged}/>
+                password <input type="password" name="password" value={this.state.password}
+                 onChange={this.handleChanged}/>   
+                <button onClick={this.loginClicked}>Loggin</button>
             </div>
         );
     }
-}
 
+
+}
 export default TodoApp;
