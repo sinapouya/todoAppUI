24b52503e17c7fe3fbcf519e0c4dc1be111a87af
sina <pooya.sina@gmail.com>
2020-08-17 20:57:05 +0430

    add todoComponent.jsx and relevant page to edit todo + add moment and formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,7 +1,45 @@
 import React,{Component} from 'react';
+import { Formik, Field, Form } from 'formik';
+import moment from 'moment';
+import { Button } from 'semantic-ui-react';
 class TodoComponent extends Component{
+    constructor(){
+        super();
+        this.state = {
+            id:22,
+            description:'description',
+            targetDate:moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
     render(){
-    return <div>todo component to edit todo id is {this.props.match.params.id}</div>
+    return <div className="mainContent">
+        {/* id is {this.props.match.params.id} */}
+        <Formik >
+            {
+                (props) => (
+                    <Form className="formClass ui form" style={{border: 'antiquewhite'}}>
+                        <fieldset>
+                            <div className="field">
+                                <label>description</label>
+                                <Field type="text" name="description"></Field>
+                            </div>
+                            <div className="field">
+                                <label>target Date</label>
+                                <Field type="date" name="targetDate"></Field>
+                            </div>
+                            <div>
+                                <button class="ui button" type="submit">Save</button>    
+                            </div>
+                            
+                        </fieldset>    
+                    </Form>
+                    
+                )
+                    
+            }
+                        
+        </Formik>
+    </div>
     };
 }
 export default TodoComponent; 
